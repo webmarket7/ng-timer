afd1ed5c7ff341bc6f0068bb399a1adf14bc0865
Oleksandr Ryzhyk <uebersetzer94@gmail.com>
2018-07-08 00:01:30 +0300

    Delete firebase-config.ts

--- src/app/firebase-config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: 'AIzaSyC-LOoyLVfWkT8mEKNrPbLl6HC4RHiryA8',
-    authDomain: 'ng-timer.firebaseapp.com',
-    databaseURL: 'https://ng-timer.firebaseio.com',
-    projectId: 'ng-timer',
-    storageBucket: 'ng-timer.appspot.com',
-    messagingSenderId: '55249725552'
-};
